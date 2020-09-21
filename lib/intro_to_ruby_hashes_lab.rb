fe31e9a3a4e9ede144f8c2921b5468bbf5b73f2b
julio hernandez <julioandres72@gmail.com>
2020-09-21 15:37:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,11 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    :my_name => "Jules",
+    :my_age => "29"
+  }
+  my_hash
 end
 
 def pioneer
